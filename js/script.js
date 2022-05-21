8e775e32959db7e750d376e3ef8969f5f7f3f048
nakano ryo <>
2022-05-21 21:46:05 +0000

    FirstCommit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+{
+    const btn = document.querySelector('.btn-menu');
+    const nav = document.querySelector('.main-nav');
+    
+    btn.addEventListener('click' , () => {
+       nav.classList.toggle('open-menu'); 
+       
+       if (btn.innerHTML === 'Menu') {
+           btn.innerHTML = 'Close';
+       } else {
+         btn.innerHTML = 'Menu';  
+       } 
+      
+    }); 
+    
+}
